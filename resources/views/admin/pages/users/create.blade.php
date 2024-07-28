c1b11b6c2c39390982a8abef0ec46a50991a872e
Khant-Nyar <khantnyar.dev@gmail.com>
2024-07-29 00:17:39 +0630

    remove some unnessarery code

--- resources/views/admin/pages/users/create.blade.php
+++ resources/views/admin/pages/users/create.blade.php
@@ -30,18 +30,10 @@
     </div>
     @push('javascript')
         <!-- Page level plugins -->
-        {{-- <script src="{{ asset('backend/vendor/datatables/jquery.dataTables.min.js') }}"></script> --}}
-        {{-- <script src="{{ asset('backend/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script> --}}
-
-        <!-- Page level custom scripts -->
-
-        <!-- Javascript Requirements -->
-        {{-- <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script> --}}
-
         <!-- Laravel Javascript Validation -->
         <script type="text/javascript" src="{{ asset('vendor/jsvalidation/js/jsvalidation.js') }}"></script>
 
+        <!-- Page level custom scripts -->
         {!! JsValidator::formRequest('App\Http\Requests\StoreUserRequest', '#store-user') !!}
         <script defer></script>
     @endpush
